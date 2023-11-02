53009aa3d37cb4429cedae62ca1541437ff7e6a0
Fiol Thiago <fiolthiago9@gmail.com>
2023-11-02 15:59:34 -0300

    Termminamos el punto 7

---
+++ js/p-7.js
@@ -0,0 +1,12 @@
+let numero = parseInt(prompt("Ingrese un número del 1 al 50"));
+if (numero < 1 || numero > 50 || isNaN(numero)) {
+  alert("Número inesperado, vuelva a intentarlo");
+} else {
+  for (let indice = numero; indice >= 1; indice--) {
+    document.write("<p>");
+    for (let indiceNum = 1; indiceNum <= indice; indiceNum++) {
+      document.write(indice);
+    }
+    document.write("</p>");
+  }
+}

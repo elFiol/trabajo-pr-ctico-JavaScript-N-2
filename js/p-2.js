7c5d1d198d6c88afffa4331c8c570e35e7f0aa1f
Fiol Thiago <fiolthiago9@gmail.com>
2023-11-01 21:25:53 -0300

    Terminamos el punto 2 de JS

---
+++ js/p-2.js
@@ -0,0 +1,20 @@
+const calificacion = parseInt(prompt("ingrese una nota de 0 a 10"));
+if (isNaN(calificacion)) {
+  alert("introduce un numero valido");
+} else if (calificacion < 0 || calificacion > 10) {
+  alert("Número erróneo");
+} else if (calificacion <= 2) {
+  alert("muy deficiente");
+} else if (calificacion <= 4) {
+  alert("insuficiente");
+} else if (calificacion <= 6) {
+  alert("suficiente");
+} else if (calificacion === 7) {
+  alert("bien");
+} else if (calificacion <= 9) {
+  alert("notable");
+} else if (calificacion === 10) {
+  alert("sobresaliente");
+} else {
+  alert("numero erróneo");
+}
